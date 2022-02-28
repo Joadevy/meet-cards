f90351eb18e572e8319980785724d19366ff70b5
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-02-28 12:22:08 -0300

    Start working with the info into the cards

--- js/index.js
+++ js/index.js
@@ -1,34 +1,42 @@
+const containerData = document.querySelector('.data');
+const btnPrevious = document.getElementById('previous');
+const btnRandom = document.getElementById('random');
+const btnNext = document.getElementById('next');
+
 const getInfo = async(role) => {
     try {
-    let request = await fetch('data/information.json')
-    let result = await request.json();
-    for (let data in result){
-        if (result[data].role == role) {
-            return result[data];
+        let request = await fetch('data/information.json');
+        let result = await request.json();
+        for (let data in result){
+            if (result[data].role == role) {
+                return result[data];
+            }
         }
-    }
-   /*  
-    .then (response => response.json())
-    .then (data => {
-        for (let element in data) {
-            if(data[element].role == role){
-                return data[element];
-            };
-        }
-    }) */
     } catch (error) {
         console.log(error);
     }
 }
 
-const showInfo = async() => {
-    let ceo = await getInfo('ceo');
-    console.table(ceo);
+const showInfo = async(role) => {
+    let info = await getInfo(role);
+    createCard(info)
+    //printInfo(info.url,info.text);
 }
 
-showInfo();
+showInfo('ceo');
+/* const printInfo = (url,text) => {
+    const image = containerData.firstElementChild.firstElementChild;
+    const paragraph = containerData.lastElementChild;
+    image.src = url;
+    paragraph.textContent = text; */
+//}
 
-/* 
-No se puede asi, es una operacion asincrona XD
-let ceo = getInfo('ceo');
-console.log(typeof ceo); */
+const createCard = (info) => {
+    let card = `<div class="img-person">
+                    <img src="${info.url}" alt="person name">
+                </div>
+                <p class="description">${info.text}</p>`;
+    containerData.innerHTML = card;
+}
+
+
