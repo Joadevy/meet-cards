85dca57de877eee46f356893965f3fec6556ede2
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-02-28 12:52:56 -0300

    Next/previous person function added and working

--- js/index.js
+++ js/index.js
@@ -2,28 +2,41 @@ const containerData = document.querySelector('.data');
 const btnPrevious = document.getElementById('previous');
 const btnRandom = document.getElementById('random');
 const btnNext = document.getElementById('next');
+let index = 0;
 
-const getInfo = async(role) => {
+btnNext.addEventListener('click', () => {
+    changePerson ('next');
+})
+
+btnPrevious.addEventListener('click', () => {
+    changePerson ('previous');
+})
+
+
+// Gets the information from the JSON using a fetch request.
+const getInfo = async() => {
     try {
-        let request = await fetch('data/information.json');
-        let result = await request.json();
-        for (let data in result){
-            if (result[data].role == role) {
-                return result[data];
-            }
-        }
+        let request = await fetch('data/information.json'); // Accesing to the returned info of the fetch.
+        let result = await request.json(); // Transforming the returned info of the fetch into JSON and accesing to the data.
+        return result
     } catch (error) {
         console.log(error);
     }
 }
 
-const showInfo = async(role) => {
-    let info = await getInfo(role);
-    createCard(info)
-    //printInfo(info.url,info.text);
+const showInfo = async(index) => {
+    let info = await getInfo();
+    let personalData;
+    /* for (let data in info){
+        if (info[data].role == role) {
+            personalData= info[data];
+        }
+    } */
+    personalData = info[index];
+    createCard(personalData)
 }
 
-showInfo('ceo');
+showInfo(0);
 /* const printInfo = (url,text) => {
     const image = containerData.firstElementChild.firstElementChild;
     const paragraph = containerData.lastElementChild;
@@ -35,8 +48,24 @@ const createCard = (info) => {
     let card = `<div class="img-person">
                     <img src="${info.url}" alt="person name">
                 </div>
+                <h2 class"name">${info.name}</h2>
+                <h3 class='role'>${info.role}</h3>
                 <p class="description">${info.text}</p>`;
     containerData.innerHTML = card;
 }
 
+const changePerson = (button) => {
+    if (button === 'next') {
+        index++;
+        if (index == 7) {
+            index = 0;
+        }
+    } else if (button === 'previous') {
+        index--;
+        if (index == -1) {
+            index = 6;
+        }
+    }
+    showInfo(index);
+}
 
